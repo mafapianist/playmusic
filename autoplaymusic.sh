eea8b7baec62955a5dddf03d930819ac10c1b89d
mrtakil <49750385+mrtakil@users.noreply.github.com>
2019-06-07 10:34:53 +0700

    Add files via upload

--- autoplaymusic.sh
+++ autoplaymusic.sh
@@ -4,19 +4,29 @@ b='\033[1m'
 
 clear
 figlet Hai Welcome To My Script Music | lolcat
-sleep 2
+sleep 3
 clear
 
-echo "✿──────────────────────✿"
+echo "Maaf Ini Harus Di Install Dan Di Setuju Karena Takut Ada Yang Masih Eror Apakah Mau?"
+echo "Jika Mau Tekan Enter"
+read enter
+
+termux-setup-storage
+pkg install ruby
+gem install lolcat
+pkg install figlet
+clear
+
+echo "✿──────────────────────────✿" | lolcat
 echo "Scripts By MrTakil (Faris)"
 echo "Script New"
 echo "Created Date : 6 Juni 2019"
-echo "Versi : 1.0"
+echo "Versi : 1.1"
 echo "Whatsaap : 089657737111"
 echo "Facebook : Shc - Faris"
 echo "Instagram : faris_nepatas007"
 echo "Youtube : shc - faris"
-echo "✿──────────────────────✿"
+echo "✿──────────────────────────✿" | lolcat
 
 ###################################################
 # CTRL + C
@@ -24,16 +34,16 @@ echo "✿──────────────────────✿"
 trap ctrl_c INT
 ctrl_c() {
 clear
-echo -e $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo -e "[#]> (Ctrl + C ) Detected, Trying To Exit ... "
 sleep 1
 echo ""
-echo -e $green"[#]> Terima kasih sudah make tools saya ... "
+echo -e "[#]> Terima kasih sudah make tools saya ... "
 sleep 1
 echo ""
-echo -e $white"[#]> MrTakil Wuzz Here ... "
+echo -e "[#]> MrTakil Wuzz Here ... "
 sleep 1
 echo ""
-echo -e $white "[#]> Teken Enter Gan! "
+echo -e "[#]> Teken Enter Gan! "
 sleep 1
 read enter
 exit
@@ -42,18 +52,16 @@ exit
 lagi=1
 while  [ $lagi -lt 4 ];
 do
-echo ""
-echo "✿─────────────────✿"
+date | lolcat
+echo "✿─────────────────✿" | lolcat
 echo -e "1.Music Download"
-echo "✿─────────────────✿"
+echo "✿─────────────────✿" | lolcat
 echo -e "2.Music Vidmate"
-echo "✿─────────────────✿"
+echo "✿─────────────────✿" | lolcat
 echo -e "3.Music UCDownload"
-echo "✿─────────────────✿"
+echo "✿─────────────────✿" | lolcat
 echo -e "4.Exit"
-echo "✿─────────────────✿"
-echo ""
-date
+echo "✿─────────────────✿" | lolcat
 echo "Pilih Nomor Yang di Sediakan"
 read -p "[MrTakil@Root]>" pil;
 
@@ -77,10 +85,16 @@ echo "Music Sudah Selesai"
 ;;
 
 4) echo "Subscribe Channel shc - faris"
+sleep 1
+echo "dan follow Instagram saya faris_nepatas007"
+sleep 1
+echo "jika ada yang eror mohon beritahu saya ke WhatsApp"
+sleep 1
 echo "bye!!!"
 exit
 ;;
 
 *) echo "Kata Kata Yang Anda Masukan Salah"
+clear
 esac
 done
